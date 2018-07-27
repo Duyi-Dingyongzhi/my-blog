7de630b3dda84a1f72461bf839ea7042cf8e9b92
dingyongzhi <yongzhi.ding@duyi-inc.com>
2018-07-27 15:31:32 +0800

    sec

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ server.get('/api/getmsg', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.end('sendMsg') 
 })
-server.listen(9000)
+server.listen(9000, () => {
+    console.log('server is running at 9000')
+})
